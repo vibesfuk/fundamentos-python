8949a208d1e9c4567647c4e3ec0a46d0feb2f1b3
Luka lopez <vibesfuk@gmail.com>
2026-07-22 10:36:26 -0400

    actualizacion ejercicio_03

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/") 
 async def root():
     return {"massage": "hello world"}
+
+@app.get("/libros") 
+async def obtenerLibros():
+    return [
+        {"id": 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"},
+        {"id": 2, "titulo": "Don Quijote de la Mancha", "autor": "Miguel de Cervantes"},
+        {"id": 3, "titulo": "El Principito", "autor": "Antoine de Saint-Exupéry"}
+    ]
